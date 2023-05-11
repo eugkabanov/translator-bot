1d187438cf8281e83248b03d387c814234c36d0e
Evgenii Kabanov <eug.kabanov@icloud.com>
2023-05-11 00:35:36 -0300

    feat: add start message

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,16 @@ from telegram.ext import ContextTypes
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    welcome_message = (
+        "Hello there! 👋\n\n"
+        "I'm your friendly Translator Bot. 🤖\n\n"
+        "I'm here to help you with translations. I can detect the language of your input "
+        "and translate it to a language of your choice. To get started, please choose your target language "
+        "by typing /language command.\n\n"
+        "Feel free to type any message, and I'll translate it for you! 🌐"
+    )
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Start message goes here"
+        chat_id=update.effective_chat.id,
+        text=welcome_message,
     )
